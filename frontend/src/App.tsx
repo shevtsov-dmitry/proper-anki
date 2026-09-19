680420afca59dc026cf6e82b394364bdad3e02e4
shevtsov-dmitry <shadowstakeus28@gmail.com>
2026-09-19 13:26:14 +0300

    feat: add drag and drop option

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
-import { AppShell } from '@mantine/core'
+import { AppShell, AppShellFooter, Center, Text } from '@mantine/core'
 import AddContent from './components/AddContent'
 
-const App = () => <AppShell>
+const App = () => <AppShell
+  style={{
+    height: "100dvh",
+    widows: "100dvw",
+  }}
+>
   <AddContent />
+  <AppShellFooter>
+    <Center>
+      <Text fz={"sm"} color='#bfbfbf'>to save the file on a server use CTRL+Enter</Text>
+    </Center>
+  </AppShellFooter>
 </AppShell>
 
 export default App
--- frontend/src/components/AddContent.tsx
+++ frontend/src/components/AddContent.tsx
@@ -12,6 +12,26 @@ const AddContent = () => {
     content: "",
   })
 
+  const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault()
+
+    const file = Array.from(event.dataTransfer.files).find((file) =>
+      file.type.startsWith("image/")
+    )
+
+    if (!file) {
+      return
+    }
+
+    const url = URL.createObjectURL(file)
+
+    editor
+      ?.chain()
+      .focus()
+      .setImage({ src: url })
+      .run()
+  }
+
   const handlePaste = (event: React.ClipboardEvent<HTMLDivElement>) => {
     const items = Array.from(event.clipboardData.items)
 
@@ -45,16 +65,41 @@ const AddContent = () => {
   }
 
   return (
-    <Box
-      style={{
-        border: "1px solid #ccc",
-        borderRadius: 8,
-        padding: 16,
-      }}
-      onPaste={handlePaste}
-    >
-      <EditorContent editor={editor} />
-    </Box>
+    <>
+      <style>
+        {`
+        .tiptap {
+          flex: 1;
+          height: 100%;
+          outline: none;
+        }
+      `}
+      </style>
+
+      <Box
+        style={{
+          border: "1px solid #ccc",
+          borderRadius: 8,
+          padding: 16,
+          minHeight: "100%",
+          display: "flex",
+          flexDirection: "column",
+          margin: "2px 5%",
+        }}
+        onPaste={handlePaste}
+        onDrop={handleDrop}
+      >
+        <EditorContent
+          editor={editor}
+          style={{
+            flex: 1,
+            minHeight: 0,
+            display: "flex",
+            flexDirection: "column",
+          }}
+        />
+      </Box>
+    </>
   )
 }
 
